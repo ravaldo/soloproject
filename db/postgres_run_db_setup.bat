8e548eb5470fe141485afde94a42736889560f8d
ravaldo <117010252+ravaldo@users.noreply.github.com>
2023-06-16 13:02:45 +0100

    Update postgres_run_db_setup.bat

--- db/postgres_run_db_setup.bat
+++ db/postgres_run_db_setup.bat
@@ -1,4 +1,4 @@
 @echo off
-psql -U postgres -d soloproject -f db_setup_postgres.sql
-psql -U postgres -d soloproject -f db_setup_inserts.sql
+psql -U postgres -d soloproject -f postgres_db_setup.sql
+psql -U postgres -d soloproject -f db_populate.sql
 pause
